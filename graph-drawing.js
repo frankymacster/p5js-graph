cbfc12768722892f38e59ecf1f95b54e1167388a
Francois <fs.sunatori@me.com>
2018-05-24 12:36:01 -0400

    changed api from proof tree to drawing

--- graph-drawing.js
+++ graph-drawing.js
@@ -4,8 +4,18 @@
 // edges array
 const NODE_RADIUS = 30;
 const TEXT_SIZE = 16;
-const SET_WIDTH = 3 * NODE_RADIUS;
-const SET_HEIGHT = 5 * NODE_RADIUS;
+
+// https://stackoverflow.com/questions/1484506/random-color-generator
+const getRandomColor = () => {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
 
 class GraphDrawing {
   constructor(nodes, edges) {
@@ -15,37 +25,40 @@ class GraphDrawing {
   }
 
   addSet(set) {
+    set.color = getRandomColor();
     this.sets.push(set);
   }
 
   draw() {
     stroke('red');
     strokeWeight(4);
-    for (var i = 0; i < this.edges.length; i++) {
+    for (let i = 0; i < this.edges.length; i++) {
       line(this.edges[i][0].x, this.edges[i][0].y, this.edges[i][1].x, this.edges[i][1].y);
     }
-
     strokeWeight(0);
+
+    for (let i = 0; i < this.sets.length; i++) {
+      fill(this.sets[i].color);
+      for (let j = 0; j < this.sets[i].nodes.length; j++) {
+        ellipse(this.sets[i].nodes[j].x, this.sets[i].nodes[j].y, NODE_RADIUS * 1.5, NODE_RADIUS * 1.5);
+      }
+    }
+
     fill(255, 204, 0);
-    for (var i = 0; i < this.nodes.length; i++) {
+    for (let i = 0; i < this.nodes.length; i++) {
       ellipse(this.nodes[i].x, this.nodes[i].y, NODE_RADIUS, NODE_RADIUS);
     }
 
     fill('white');
     textSize(TEXT_SIZE);
-    for (var i = 0; i < this.nodes.length; i++) {
+    for (let i = 0; i < this.nodes.length; i++) {
       text(this.nodes[i].text, this.nodes[i].x - TEXT_SIZE / 4, this.nodes[i].y + TEXT_SIZE / 4);
     }
 
-    fill('blue');
-    for (var i = 0; i < this.sets.length; i++) {
-      ellipse(this.sets[i].x, this.sets[i].y, SET_WIDTH, SET_HEIGHT);
-    }
-
-    fill('white');
-    textSize(TEXT_SIZE);
-    for (var i = 0; i < this.sets.length; i++) {
-      text(this.sets[i].text, this.sets[i].x - TEXT_SIZE / 2, this.sets[i].y + TEXT_SIZE / 4);
-    }
+    // fill('white');
+    // textSize(TEXT_SIZE);
+    // for (var i = 0; i < this.sets.length; i++) {
+    //   text(this.sets[i].text, this.sets[i].x - TEXT_SIZE / 2, this.sets[i].y + TEXT_SIZE / 4);
+    // }
   }
 }
--- proofs/connectivity.js
+++ proofs/connectivity.js
@@ -1,196 +1,195 @@
+const getPos = pos => {
+  return pos * 50;
+};
+
+const findByText = (node, text) => {
+  return node.find(n => n.text === text);
+}
+
 const getProof = () => {
-  const proof31Nodes = [{
+  const nodes = [{
+    text: 'n',
+    x: getPos(3),
+    y: getPos(2),
+  }, {
     text: 'n1',
-    x: 50,
-    y: 25,
+    x: getPos(1),
+    y: getPos(1),
+  }, {
+    text: 'n2',
+    x: getPos(5),
+    y: getPos(1),
   }, {
     text: 'n3',
-    x: 50,
-    y: 75,
-  }, {
-    text: 'n',
-    x: 150,
-    y: 50,
-  }, {
-    text: 'n2',
-    x: 250,
-    y: 50,
-  }];
-  const proof32Nodes = [{
-    text: 'n1',
-    x: 50,
-    y: 50,
-  }, {
-    text: 'n',
-    x: 150,
-    y: 50,
-  }, {
-    text: 'n2',
-    x: 250,
-    y: 25,
-  }, {
-    text: 'n3',
-    x: 250,
-    y: 75,
-  }];
-  const proof33Nodes = [{
-    text: 'n1',
-    x: 50,
-    y: 50,
-  }, {
-    text: 'n',
-    x: 150,
-    y: 50,
-  }, {
-    text: 'n2',
-    x: 250,
-    y: 50,
-  }, {
-    text: 'n3',
-    x: 150,
-    y: 150,
-  }];
-  const proof34Nodes = [{
-    text: 'n1',
-    x: 50,
-    y: 25,
-  }, {
-    text: 'n',
-    x: 150,
-    y: 25,
-  }, {
-    text: 'n2',
-    x: 250,
-    y: 25,
+    x: getPos(3),
+    y: getPos(3),
   }, {
     text: 'n31',
-    x: 50,
-    y: 75,
-  }, {
-    text: 'n3',
-    x: 150,
-    y: 75,
+    x: getPos(1),
+    y: getPos(4),
   }, {
     text: 'n32',
-    x: 250,
-    y: 75,
+    x: getPos(5),
+    y: getPos(4),
   }];
-  const proof1 = [{
-    id: '0',
+
+  const proof = [{
+    id: '1',
     text: 'graphs.regular.3 &ni; G<br />&nbsp; ...<br />&nbsp; &nbsp; G.connectivity.node = G.connectivity.edge',
   }, {
-    id: '01',
-    text: 'G.nodes &supe; S<br />&nbsp; S &ni; n<br />&nbsp; &nbsp; n.neighbors &ni; n1<br />&nbsp; &nbsp; &nbsp; H1 &ni; n1<br />&nbsp; &nbsp; &nbsp; &nbsp; n.neighbors &ni; n2<br />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; H2 &ni; n2<br />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; n.neighbors &ni; n3',
+    id: '11',
+    text: 'G.nodes &ni; n',
   }, {
-    id: '011',
+    id: '111',
+    text: 'n.neighbors &ni; n1<br />&nbsp; n.neighbors &ni; n2<br />&nbsp; &nbsp; n.neighbors &ni; n3',
+  }, {
+    id: '1111',
+    text: 'H2 &ni; n2<br />&nbsp; H1 &ni; n1',
+  }, {
+    id: '11111',
     text: 'H1 &ni; n3',
   }, {
-    id: '012',
+    id: '11112',
     text: 'H2 &ni; n3',
   }, {
-    id: '013',
+    id: '11113',
     text: 'H3 &ni; n3',
   }, {
-    id: '014',
+    id: '11114',
     text: 'S &ni; n3',
   }, {
-    id: '015',
+    id: '11115',
     text: '&or;<br /><p>&nbsp; S\'.# = S.#<br />&nbsp; &nbsp; &nbsp; G.connectivity.node = G.connectivity.edge',
   }, {
-    id: '0111',
+    id: '111111',
     text: '&nbsp; graphs.&not;connected &ni; G - {{n, n2}}',
     drawing: {
-      nodes: proof31Nodes,
+      nodes: [
+        findByText(nodes, 'n'),
+        findByText(nodes, 'n1'),
+        findByText(nodes, 'n2'),
+        findByText(nodes, 'n3'),
+      ],
 
       edges: [
-        [proof31Nodes[0], proof31Nodes[2]],
-        [proof31Nodes[1], proof31Nodes[2]],
-        [proof31Nodes[2], proof31Nodes[3]],
+        [findByText(nodes, 'n'), findByText(nodes, 'n1')],
+        [findByText(nodes, 'n'), findByText(nodes, 'n2')],
+        [findByText(nodes, 'n'), findByText(nodes, 'n3')],
       ],
 
       sets: [{
         text: 'H1',
-        x: 50,
-        y: 50,
+        nodes: [
+          findByText(nodes, 'n1'),
+          findByText(nodes, 'n3'),
+        ],
       }, {
         text: 'H2',
-        x: 250,
-        y: 50,
+        nodes: [
+          findByText(nodes, 'n2'),
+        ],
       }],
     },
   }, {
-    id: '0121',
+    id: '111121',
     text: '&nbsp; graphs.&not;connected &ni; G - {{n, n1}}',
     drawing: {
-      nodes: proof32Nodes,
+      nodes: [
+        findByText(nodes, 'n'),
+        findByText(nodes, 'n1'),
+        findByText(nodes, 'n2'),
+        findByText(nodes, 'n3'),
+      ],
 
       edges: [
-        [proof32Nodes[0], proof32Nodes[1]],
-        [proof32Nodes[1], proof32Nodes[2]],
-        [proof32Nodes[1], proof32Nodes[3]],
+        [findByText(nodes, 'n'), findByText(nodes, 'n1')],
+        [findByText(nodes, 'n'), findByText(nodes, 'n2')],
+        [findByText(nodes, 'n'), findByText(nodes, 'n3')],
       ],
 
       sets: [{
         text: 'H1',
-        x: 50,
-        y: 50,
+        nodes: [
+          findByText(nodes, 'n1'),
+        ],
       }, {
         text: 'H2',
-        x: 250,
-        y: 50,
+        nodes: [
+          findByText(nodes, 'n2'),
+          findByText(nodes, 'n3'),
+        ],
       }],
     },
   }, {
-    id: '0131',
+    id: '111131',
     text: '&nbsp; graphs.&not;connected &ni; G - {{n, n1}}<br />&nbsp; graphs.&not;connected &ni; G - {{n, n2}}<br />&nbsp; graphs.&not;connected &ni; G - {{n, n3}}<br />&nbsp; &or;',
     drawing: {
-      nodes: proof33Nodes,
+      nodes: [
+        findByText(nodes, 'n'),
+        findByText(nodes, 'n1'),
+        findByText(nodes, 'n2'),
+        findByText(nodes, 'n3'),
+      ],
 
       edges: [
-        [proof33Nodes[0], proof33Nodes[1]],
-        [proof33Nodes[1], proof33Nodes[2]],
-        [proof33Nodes[1], proof33Nodes[3]],
+        [findByText(nodes, 'n'), findByText(nodes, 'n1')],
+        [findByText(nodes, 'n'), findByText(nodes, 'n2')],
+        [findByText(nodes, 'n'), findByText(nodes, 'n3')],
       ],
 
       sets: [{
         text: 'H1',
-        x: 50,
-        y: 50,
+        nodes: [
+          findByText(nodes, 'n1'),
+        ],
       }, {
         text: 'H2',
-        x: 250,
-        y: 50,
+        nodes: [
+          findByText(nodes, 'n2'),
+        ],
       }, {
         text: 'H3',
-        x: 150,
-        y: 200,
+        nodes: [
+          findByText(nodes, 'n3'),
+        ],
       }],
     },
   }, {
-    id: '0141',
+    id: '111141',
     text: '&nbsp; n3.neighbors &ni; n31<br />&nbsp; &nbsp; H1 &ni; n31<br />&nbsp; &nbsp; &nbsp; n3.neighbors &ni; n32<br />&nbsp; &nbsp; &nbsp; &nbsp; H2 &ni; n32<br />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; graphs.&not;connected &ni; G - {{n, n1}, {n3, n31}}<br />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; graphs.&not;connected &ni; G - {{n, n2}, {n3, n32}}<br />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &or;',
     drawing: {
-      nodes: proof34Nodes,
+      nodes: [
+        findByText(nodes, 'n'),
+        findByText(nodes, 'n1'),
+        findByText(nodes, 'n2'),
+        findByText(nodes, 'n3'),
+        findByText(nodes, 'n31'),
+        findByText(nodes, 'n32'),
+      ],
 
       edges: [
-        [proof34Nodes[0], proof34Nodes[1]],
-        [proof34Nodes[1], proof34Nodes[2]],
-        [proof34Nodes[3], proof34Nodes[4]],
-        [proof34Nodes[4], proof34Nodes[5]],
-        [proof34Nodes[1], proof34Nodes[4]],
+        [findByText(nodes, 'n'), findByText(nodes, 'n1')],
+        [findByText(nodes, 'n'), findByText(nodes, 'n2')],
+        [findByText(nodes, 'n'), findByText(nodes, 'n3')],
+        [findByText(nodes, 'n3'), findByText(nodes, 'n31')],
+        [findByText(nodes, 'n3'), findByText(nodes, 'n32')],
       ],
 
       sets: [{
         text: 'H1',
-        x: 50,
-        y: 50,
+        nodes: [
+          findByText(nodes, 'n1'),
+          findByText(nodes, 'n31'),
+        ],
       }, {
         text: 'H2',
-        x: 250,
-        y: 50,
+        nodes: [
+          findByText(nodes, 'n2'),
+          findByText(nodes, 'n32'),
+        ],
       }],
     },
   }];
 
-  return proof1;
+  return proof;
 };
